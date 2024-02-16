6e7ea6fd590952314e8cd1bd510070de9af73fd6
Marvin <okongomarvin971@gmail.com>
2024-02-16 13:43:13 +0300

    Home image

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,17 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import AboutUs from './components/Pages/AboutUs';
-// import ContactUs from './components/Pages/ContactUs';
-
-import './NavBar.css'; // Import the CSS file
+import './NavBar.css';
 
 const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="logo">mdishi App</div>
+      <div className='Navlinks'>
       <Link to="/" className="nav-link">Home</Link>
       <Link to="/about" className="nav-link">About Us</Link>
       <Link to="/contact" className="nav-link">Contact Us</Link>
+      </div>
 
     </nav>
   );
